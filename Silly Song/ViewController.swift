b8ffcee8ca720aa0a1a7bb4e52934cfd292e7cca
Katerina Fedoseeva <katie_fed@hotmail.com>
2017-07-30 13:42:58 -0700

    Initial Commit

---
+++ Silly Song/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Katie Fedoseeva on 7/30/17.
+//  Copyright © 2017 Katie Fedoseeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
